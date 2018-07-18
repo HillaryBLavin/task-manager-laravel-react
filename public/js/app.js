a427424ffa3f4ba0e5fae7925580b196ffc86c28
Hillary Lavin <hilavin@gmail.com>
2018-07-18 18:53:56 -0400

    Add setState to handleSubmit - adds new task to tasks array, resets placeholder text

--- public/js/app.js
+++ public/js/app.js
@@ -55761,6 +55761,8 @@ module.exports = camelize;
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -55798,11 +55800,17 @@ var App = function (_Component) {
     }, {
         key: 'handleSubmit',
         value: function handleSubmit(event) {
+            var _this2 = this;
+
             event.preventDefault();
             axios.post('/tasks', {
                 name: this.state.name
             }).then(function (response) {
-                console.log('from handle submit', response);
+                // console.log('from handle submit', response);
+                _this2.setState({
+                    tasks: [response.data].concat(_toConsumableArray(_this2.state.tasks)),
+                    name: ''
+                });
             }).catch(function (error) {
                 console.log(error.response);
             });
